ab37cce01af2b8dc0c2b92fbc6fac97fb1380fdd
Marco Sanchez <marcosanchez.p95@gmail.com>
2025-06-03 10:43:18 -0600

    Proyecto_v2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # proyecto
 Proyecto sprint 7
+
+Esta es una aplicación web desarrollada con Streamlit que permite visualizar datos de vehículos en venta en Estados Unidos.
+
+Funciones
+
+-Visualización de un histograma de la columna "odometer" (kilometraje).
+-Visualización de un gráfico de dispersión entre "odometer" y "price" (precio del coche).
+-Interacción mediante botones o casillas de verificación para mostrar los gráficos.
+-Gráficos generados con Plotly express para una visualización interactiva.
---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+#leer archivo
+car_data = pd.read_csv(r"C:\Users\marco.paez\OneDrive - NEORIS\Desktop\tripleten\Proyecto_sprint7\proyecto\Notebooks\vehicles_us.csv")
+
+hist_button = st.button('Construir histograma') # crear un botón
+     
+if hist_button: # al hacer clic en el botón
+         # escribir un mensaje
+         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear un histograma
+         fig = px.histogram(car_data, x="odometer")
+     
+         # mostrar un gráfico Plotly interactivo
+         st.plotly_chart(fig, use_container_width=True)
+
+# Casilla para diagrama de dispersión
+build_scatter = st.checkbox('Mostrar gráfico de dispersión: odómetro vs precio')
+if build_scatter:
+    st.write('Gráfico de dispersión entre odómetro y precio')
+    fig = px.scatter(car_data, x="odometer", y="price", title="Odómetro vs Precio")
+    st.plotly_chart(fig, use_container_width=True)
